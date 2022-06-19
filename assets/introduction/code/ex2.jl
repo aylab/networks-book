eb564fc182634d09c99b7005cf19c6375e1d1a0a
syedtaz <58488504+syedtaz@users.noreply.github.com>
2022-06-19 08:25:54 +0000

    Deploying to gh-pages from  @ 9e3d27c52a55185d429521596fa16180b46ddfe1 🚀

--- assets/introduction/code/ex2.jl
+++ assets/introduction/code/ex2.jl
@@ -3,14 +3,20 @@
 using GraphRecipes
 using Plots
 
-const B = [0 1 1; 1 0 0; 1 0 0]
+const n = 22
+const A = Float64[ rand() < 0.65 ? 0 : rand() for i=1:n, j=1:n]
+for i=1:n
+    A[i, 1:i-1] = A[1:i-1, i]
+    A[i, i] = 0
+end
 
-graphplot(B,
+graphplot(A,
           markersize = 0.2,
+          node_weights = 1:n,
           markercolor = range(colorant"yellow", stop=colorant"red", length=n),
-          names = 1:3,
+          names = 1:n,
           fontsize = 10,
           linecolor = :darkgrey
           )
 
-savefig(joinpath(@OUTPUT, "simple_graph.png"))
+savefig(joinpath(@OUTPUT, "test.png"))
